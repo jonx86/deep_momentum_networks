8b8701d85bc6aaf168796dc3361a7b1c2750a539
Masa Kuramoto <masadkuramoto@gmail.com>
2023-12-06 20:40:29 -0800

    scaled the test dataset

--- main_lstm_optuna.py
+++ main_lstm_optuna.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import copy
+from datetime import datetime
+from pytz import timezone
+import pytz
 
 from sklearn.preprocessing import RobustScaler
 
@@ -44,32 +47,56 @@ loss_func_name = sys.argv[2]
 gpu = sys.argv[3]
 n_trials = int(sys.argv[4])
 n_jobs = int(sys.argv[5])
+max_epochs = int(sys.argv[6])
+sampler_type = sys.argv[7]
 
 batch_size_space = [256, 512, 1024, 2048]
 learning_rate_space = [10**-5, 10**-4, 10**-3, 10**-2, 10**-1, 10**0]
 maximum_gradient_norm_space = [10**-4, 10**-3, 10**-2, 10**-1, 10**0, 10**1]
 hidden_size_space = [5, 10, 20, 40, 80]
 dropout_space = [0.1, 0.2, 0.3, 0.4, 0.5]
+weight_space = [0.6, 0.55, 0.5, 0.45, 0.4]
 
-print(model_name, loss_func_name, gpu, n_trials, n_jobs)
-filename = model_name + "_" + loss_func_name
-outfile = open("results/" + filename + ".txt", "w")
-outfile_best_param = open("results/best_params_" + filename + ".txt", "w")
+batch_size_space = [256]
+learning_rate_space = [1e-3]
+maximum_gradient_norm_space = [.001]
+hidden_size_space = [40]
+dropout_space = [0.3]
+weight_space = [0.5]
+
+print(model_name, loss_func_name, gpu, n_trials, n_jobs, max_epochs)
+
+##########################################################
+# Create directory to store data
+##########################################################
+filename = model_name + "_" + loss_func_name + "_" + sampler_type + "_" + str(max_epochs)
+
+now = datetime.now(tz=pytz.utc)
+now = now.astimezone(timezone('US/Pacific'))
+data_str = now.strftime("%m%d") + "_" + now.strftime('%H%M%S')
+loc_files = "files/" + filename + "_" + data_str + "/"
+if not os.path.exists(loc_files):
+    os.makedirs(loc_files)
+
+outfile = open(loc_files + filename + ".txt", "w")
+outfile_best_param = open(loc_files + "best_params_" + filename + ".txt", "w")
 model_name = model_name + loss_func_name
 
-# Parameters
-model_path = 'model_weights/' + filename + '_model_'
+model_path = loc_files + filename + '_model_'
 cv_global = 0
+
 ##########################################################
-# Training
+# Tuning
 ##########################################################
 
 start_time = time.time()
 
 def run_train(params):
+    global global_val_loss
+    global learning_curves_global
 
     batch_size = params['batch_size']
-    epochs = 100
+    epochs = max_epochs
     early_stopping = 25
     input_dim = 10
     learning_rate = params['learning_rate']
@@ -99,10 +126,14 @@ def run_train(params):
     optimizer = Adam(model.parameters(), lr=learning_rate)
     scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=.5, verbose=False)
     loss_func = getattr(L, loss_func_name)
-    loss_func = loss_func()
+    if loss_func_name == "SharpeLossCustom":
+        loss_func = loss_func(params['weight'])
+    else:
+        loss_func = loss_func()
 
     early_stop_count = 0
     best_val_loss = float('inf')
+    learning_curves = pd.DataFrame(columns=['train_loss', 'val_loss'])
     for epoch in range(epochs):
         train_loss = train_model(epoch,
                                  model,
@@ -129,13 +160,13 @@ def run_train(params):
         else:
             early_stop_count += 1
 
-        global global_val_loss
         if val_loss < global_val_loss:
             global_val_loss = val_loss
             best_model = copy.deepcopy(model)
             torch.save(best_model, model_path + str(idx) + '.pt')
             best_model_state = copy.deepcopy(best_model.state_dict())
             torch.save(best_model_state, model_path + str(idx) + '_state_dict.pt')
+            learning_curves_global = learning_curves
 
         # print(epoch, "Training Loss: %.4f. Validation Loss: %.4f. " % (train_loss, val_loss))
 
@@ -160,12 +191,18 @@ def objective(trial):
         'dropout': dropout,
     }
 
+    if loss_func_name == "SharpeLossCustom":
+        weight = trial.suggest_categorical('weight', weight_space)
+        params['weight'] = weight
+
     best_val_loss = run_train(params)
     return best_val_loss
 
 def run_hyper_parameter_tuning():
-    # sampler = optuna.samplers.TPESampler(seed=42)
-    sampler = optuna.samplers.RandomSampler(seed=0)
+    if sampler_type == 'TPE':
+        sampler = optuna.samplers.TPESampler(seed=0)
+    elif sampler_type == 'Random':
+        sampler = optuna.samplers.RandomSampler(seed=0)
     study = optuna.create_study(sampler=sampler)
     study.optimize(objective, n_trials=n_trials, n_jobs=n_jobs, show_progress_bar=True)
 
@@ -206,6 +243,7 @@ try:
         newX = pickle.load(f)
         print(f"Loading pickle took: {time.time() - start}")
 except Exception:
+    print('creating pickle .....')
     newX, _ = split_Xy_for_seq(X[features], X['target'],
                                step_size=SEC_LEN,
                                return_seq_target=True,
@@ -218,7 +256,7 @@ except Exception:
 predictions = []
 for idx, (train, test) in enumerate(get_cv_splits(X)):
     print("cv run:", idx)
-    learning_curves = pd.DataFrame(columns=['train_loss', 'val_loss'])
+    learning_curves_global = pd.DataFrame(columns=['train_loss', 'val_loss'])
     iter_time = AverageMeter()
     train_losses = AverageMeter()
 
@@ -260,20 +298,14 @@ for idx, (train, test) in enumerate(get_cv_splits(X)):
     model.load_state_dict(torch.load(model_path + str(idx) + '_state_dict.pt'))
     model.to(gpu)
 
-    learning_curves.plot()
+    learning_curves_global.plot()
     plt.title(f'CV Split: {idx}')
-    plt.savefig('images/' + filename + f'_learning_curves_{idx}.png')
+    plt.savefig(loc_files + filename + f'_learning_curves_{idx}.png')
     plt.clf()
 
     # export the best parameters
-    print('cv: ', cv_global, file=outfile_best_param)
+    print('cv:', cv_global, 'best_val_loss:', global_val_loss, file=outfile_best_param)
     print(best_params, file=outfile_best_param, flush=True)
-
-    X_test2 = X_test.copy()
-    X_test2 = scaler.transform(X_test2)
-
-    X_test2 = X_test.copy()
-    X_test2 = retain_pandas_after_scale(X_test2, scaler)
 
     # we need a tuple of the test set start and end dates
     test_start, test_end = X_test.index.get_level_values('date')[0], X_test.index.get_level_values('date')[-1]
@@ -283,37 +315,49 @@ for idx, (train, test) in enumerate(get_cv_splits(X)):
     # futures and all over-lapping sequences with in each future
     # We utilize joblib again to multi-process these batches, looping through sequences within each future
     # takes ~1.6 minutes on my machine 24-cores
-    xs1 = mpSplits(prep.split_single_future,
-                   (test_start, test_end),
-                   newX, n_jobs=NUM_CORES)
+    # xs1 = mpSplits(prep.split_single_future,
+    #                (test_start, test_end),
+    #                newX, n_jobs=NUM_CORES)
+
+    FILENAME = f"xs1_{SEC_LEN}_{idx}.pickle"
+    try:
+        with open(FILENAME, "rb") as f:
+            print('loading xs1 pickle .....')
+            start = time.time()
+            xs1 = pickle.load(f)
+            print(f"Loading pickle took: {time.time() - start}")
+    except Exception:
+        print('creating xs1 pickle .....')
+        xs1 = mpSplits(prep.split_single_future,
+                       (test_start, test_end),
+                       newX, n_jobs=NUM_CORES)
+
+        with open(FILENAME, "wb") as f:
+            pickle.dump(xs1, f)
+            print("dumped file")
 
     with torch.no_grad():
         model.eval()
         # feed in sequences for each future and get the predictions, take just the last time-step
-        preds = aggregate_seq_preds(model, xs1,
-                                    features=features,
-                                    device=gpu,
-                                    lstm=True,
-                                    seq_out=True,
+        preds = aggregate_seq_preds(model, scaler, xs1, features=features, device=gpu, lstm=True, seq_out=True,
                                     n_jobs=2)
-
         preds = preds.to_frame(model_name)
         predictions.append(preds)
 
     cv_global += 1
     preds=pd.concat([predictions[idx]]).sort_index()
     new_dataset = data.copy()
-    feats = new_dataset.join(preds[[model_name]], how='left')
+    feats = new_dataset.join(preds[model_name], how='left')
     feats.dropna(subset=[model_name], inplace=True)
     dates = feats.index.get_level_values('date').unique().to_list()
     strat_rets = process_jobs(dates, feats, signal_col=model_name)
     values = get_returns_breakout(strat_rets.fillna(0.0).to_frame(model_name + '_bench'))
-    print('idx: ', idx, file=outfile)
+    print('idx:', idx, 'best_val_loss:', global_val_loss, file=outfile)
     print(values, file=outfile, flush=True)
     print(values)
 
 preds=pd.concat(predictions).sort_index()
-feats = data.join(preds[[model_name]], how='left')
+feats = data.join(preds[model_name], how='left')
 feats.dropna(subset=[model_name], inplace=True)
 dates = feats.index.get_level_values('date').unique().to_list()
 strat_rets = process_jobs(dates, feats, signal_col=model_name)
@@ -325,9 +369,10 @@ print(ret_breakout, file=outfile, flush=True)
 outfile.close()
 outfile_best_param.close()
 
-strat_rets.to_pickle("results/strat_rets_" + filename + ".pkl")
+strat_rets.to_pickle(loc_files + "strat_rets_" + filename + ".pkl")
 
 ax = strat_rets.fillna(0.).cumsum().plot()
-ax.figure.savefig('images/' + filename + '_timeline.png')
+ax.figure.savefig(loc_files + filename + '_timeline.png')
 
+print(data_str)
 print("total time:", (time.time() - start_time) / 60)
